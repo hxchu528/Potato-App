4ea1dec0564a394e58d077d64b29463c2798946c
Jerry Chu <hxchu528@hotmail.com>
2026-04-04 15:23:19 -0500

    Updated potato app

--- PotatoApp/app/views.py
+++ PotatoApp/app/views.py
@@ -2,21 +2,89 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import JsonResponse
 from django.template import loader
-import json
-
+import csv
+from io import TextIOWrapper
+from datetime import datetime
+import pandas as pd
+from xgboost import XGBRegressor
+import joblib
+import os
 
 # Create your views here.
 def members(request):
     template = loader.get_template('potatoApp.html')
     return HttpResponse(template.render())
 
+def FtoC(fahrenheit):
+    celsius = (fahrenheit - 32) * 5.0/9.0
+    return celsius
+def toMM(inches):
+    mm = inches * 25.4
+    return mm
+
+weather = []
 def calculate(request):
     if request.method == "POST":
-        data = request.FILES.get("weather")
-        for chunk in data.chunks():
-            print(chunk)
-        # Perform calculations or processing with the received data
-        result = {"message": "Data received successfully!"}
+        #print(len(request.FILES))
+        #print(list(request.POST))
+        for key in request.POST:
+            if key.startswith("irrigation_date_"):
+                print(request.POST.get(key))
+            if key.startswith("irrigation_amount_"):
+                print(request.POST.get(key))
+        
+        weather_data = request.FILES.get("weather")
+        weather_decoded_file = TextIOWrapper(weather_data.file, encoding='utf-8')
+        weather_reader = csv.reader(weather_decoded_file)
+        for row in weather_reader:
+            weather.append(row)
+        for i in range(5):
+            weather.pop(0)
+        planting_date = request.POST.get("planting_date")
+        planting_date = datetime.strptime(planting_date, "%Y-%m-%d").date()
+        spad_date = request.POST.get("spad_date")
+        spad_date = datetime.strptime(spad_date, "%Y-%m-%d").date()
+        accGDD = 0
+        accMoist = 0
+        for row in weather:
+            date_str = row[4]+"-"+row[5]+"-"+row[6]
+            date = datetime.strptime(date_str, "%Y-%m-%d").date()
+            if date >= planting_date and date <= spad_date:
+                tmax = FtoC(float(row[7]))
+                tmin = FtoC(float(row[9]))
+                gdd = (tmax + tmin) / 2 - 10
+                if gdd < 0:
+                    gdd = 0
+                accGDD += gdd
+                precip = toMM(float(row[11]))
+                accMoist += precip
+        irrigation_value = request.POST.get("irrigation")
+        if irrigation_value:
+            accMoist += toMM(float(irrigation_value))
+        
+        script_dir = os.path.dirname(os.path.abspath(__file__))
+        model_path = os.path.join(script_dir, "final_xgb_model1.pkl")
+        scaler_path = os.path.join(script_dir, "scaler1.pkl")
+        model = joblib.load(model_path)
+        scaler = joblib.load(scaler_path)
+        cultivar_options = ['Alpine Russet', 'Clearwater', 'Dakota Russet', 'Easton','FL 1867', 'FL 2137', 'Hamlin', 'Ivory Russet', 'Lamoka','MN13142', 'MN13142Cold', 'Red Norland', 'Russet Burbank', 'Umatilla']
+        expected_columns = ['acGDDs', 'acMoist', 'SPAD'] + \
+                   [f'Cultivar_{c.replace(" ", "_")}' for c in cultivar_options[1:]]
+        cultivar = request.POST.get("cultivar")
+        cultivar_data = {f'Cultivar_{c.replace(" ", "_")}': 0 for c in cultivar_options}
+        cultivar_data[f'Cultivar_{cultivar.replace(" ", "_")}'] = 1
+        input_data = {
+            "accGDD": [accGDD],
+            "accMoist": [accMoist],
+            "SPAD": [request.POST.get("spad_value")]
+        }
+        input_data.update(cultivar_data)
+        input_df = pd.DataFrame(input_data)
+        input_df = input_df.reindex(columns=expected_columns, fill_value=0)
+        input_df_scaled = scaler.transform(input_df.values)
+        prediction = model.predict(input_df_scaled)
+        
+        result = {"message": prediction.tolist()}
         return JsonResponse(result)
     else:
         return JsonResponse({"error": "Invalid request method."}, status=400)
--- README.md
+++ README.md
@@ -1,5 +1,20 @@
 # Potato App
 ## How to run
-1. Clone the repo onto your local computer
-2. Find and open the directory on your local computer.
-3. Click the "potatoApp.html", and open the webpage in your preffered browser.
+1. Make sure that you have Python 3 installed, preferably at least Python 3.3. All Python 3.1x versions counts as greater than Python 3.3 and will work just as well. If not downloaded, go to https://www.python.org/downloads and select your preferred version.
+2. Clone the repo
+3. You will be using Python's venv. Open the terminal and move into the repo's root directory.
+![root](./img/root.png)
+3. Type this into the terminal: ```python -m venv name```.
+The ```name``` can be whatever you choose. You will reuse this venv whenever you run the app.
+![Creating venv](img/create_venv.png)
+![venv location](img/venv_location.png)
+Location of the venv underlined in red.
+4. Start the venv. If you are on Windows (either through command prompt or powershell), run ```.\name\Scripts\Activate```. If you are on macOS/Linux, run ```source name/bin/activate```.
+![venv start](./img/venv_start.png)
+5. If this is your first time running the app, run ```pip install -r requirements.txt```. This may take a while to run. Otherwise skip this step if already done previously.
+![downloading third party libraries](./img/pip_install.png)
+6. To start the server, move into the potatoApp directory and run ```python manage.py runserver```.
+![run server](./img/server_start.png)
+7. Open up the url ```http://127.0.0.1:8000/app``` in your preferred browser. From here, you may use the app.
+8. Once you are done using the app, press ```Ctrl + C``` to stop the server. Then run ```deactivate``` from the terminal to stop the venv.
+![deactivating server](./img/deactivate.png)
